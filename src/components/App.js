5cf87d9fbb4368c3d22cdd7e00b1bb38f25c0673
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-10-28 18:17:49 +0100

    axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className='ui container' style={{marginTop: '20px'}}>
-            <SearchBar />
-        </div>
-    );
+class App extends React.Component  {
+    onSearchSubmit(term) {
+        axios.get( 'https://api.unsplash.com/search/photos' , {
+                params: { query: term },
+                headers: {
+                    Authorization: 'Client-ID 6LWnOiAtmFaNPFxkskODUhLpdAN02ITJkdZSv9n3SDI'
+                }
+        });
+
+        
+    }
+
+
+    render () {
+        return (
+            <div className='ui container' style={{marginTop: '20px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        )
+    }
+    
 };
 
 
