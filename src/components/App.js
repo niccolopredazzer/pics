9fb7a06b3a48a38230c7ad1695bf596c89012809
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-10-28 21:20:45 +0100

    immagini

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 
-class App extends React.Component  {
-    onSearchSubmit(term) {
-        axios.get( 'https://api.unsplash.com/search/photos' , {
-                params: { query: term },
-                headers: {
-                    Authorization: 'Client-ID 6LWnOiAtmFaNPFxkskODUhLpdAN02ITJkdZSv9n3SDI'
-                }
-        });
+class App extends React.Component {
+  state = { images: [] };
 
-        
-    }
+  onSearchSubmit = async term => {
+    const response = await unsplash.get('/search/photos', {
+      params: { query: term }
+    });
 
+    this.setState({ images: response.data.results });
+  };
 
-    render () {
-        return (
-            <div className='ui container' style={{marginTop: '20px'}}>
-                <SearchBar onSubmit={this.onSearchSubmit} />
-            </div>
-        )
-    }
-    
-};
-
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '10px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
+      </div>
+    );
+  }
+}
 
 export default App;
